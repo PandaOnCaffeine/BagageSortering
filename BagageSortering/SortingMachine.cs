d3f8d7b841d6cdc7e5787ecf74908c6857a4c37b
PandaOnCaffeine <110375288+PandaOnCaffeine@users.noreply.github.com>
2024-01-26 15:18:31 +0100

    Update SortingMachine.cs

--- BagageSortering/SortingMachine.cs
+++ BagageSortering/SortingMachine.cs
@@ -11,44 +11,28 @@ namespace BagageSortering
 {
     internal class SortingMachine
     {
-        private Faker _nameGen = new Faker();
+        private Gates[] _gates;
+        
         private Buffer _queue;
         private TextBox _box;
-        private Random _random = new Random();
         private bool _open = true;
-        public Check_In(Buffer queue, TextBox box)
+        public Check_In(Buffer queue, Gates[] gates, TextBox box)
         {
             _queue = queue;
             _box = box;
+            _gates = gates;
         }
         public void Run()
-        {
-            int key = 1;
-
+        {         
             while (_open)
             {
-                int amount = _random.Next(1, 4);
-                Person person = new Person(_nameGen, amount);
-
-                TakeBagage(person, key);
-                Thread.Sleep(_random.Next(1500,5000));
+                _queue.Split(Gates[_queue.Next()]);                              
             }
         }
+
         public void Stop()
         {
             _open = false;
-        }
-
-        private void TakeBagage(Person person, int key)
-        {
-            int randomNr = _random.Next(0, 3);
-            for (int i = 0; i < person.BagageAmount; i++)
-            {
-                Bagage bagage = new Bagage(person.Name, randomNr, key);
-                _queue.Produce(bagage);
-                key++;
-            }
-            _box.WriteAt($"{person.Name}|Bagages:{person.BagageAmount}|Gate:{randomNr}| Check-in {_queue.Name}", ConsoleColor.Green);
-        }
+        }       
     }
 }
